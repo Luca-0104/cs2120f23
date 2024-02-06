34a321b6e0d8d6f42c91a492062b7f96f85e2b25
LiuZhe <lzhe72537@gmail.com>
2024-02-06 03:35:51 +0000

    finished hw3

--- Student/hw1.lean
+++ Student/hw1.lean
@@ -1,17 +1,39 @@
 -- git pull upstream main
 -- if 1st command can't work, run "git remote add upstream https://github.com/kevinsullivan/cs2120f23"
 
-def compose4 {α : Type}:(α → α)->(α → α)
-| f=>fun a=>(f ∘ f ∘ f ∘ f) a
+def compose4 {α : Type}: (α → α) → (α → α)
+| f => fun a => (f ∘ f ∘ f ∘ f) a
 
-fun a means function takes argument a return ()
+-- fun a means function takes argument a return ()
 
-def compn {α : Type}:(α → α)-> Nat ->(α → α)
-| n, f=>fun a=>
-| Nat.zero, f => lamda a=>a
-|(Nat.success n'),f=>(λ a=>f(compn n' f a))
 
+/-
+g after f compositions fuction
+-/
+def comp' (α β γ : Type) : (β → γ) → (α → β) → (α → γ)
+| g, f => fun a => g (f a)
+-- use function composition notation
+def comp'' (α β γ : Type) : (β → γ) → (α → β) → (α → γ)
+| g, f => g ∘ f
+
+/-!
+Takes a function and a Nat, return a function
+Use function composition notation ∘
+-/
+def compn' {α : Type} : Nat → (α → α) → (α → α)
+| Nat.zero, f => λ a => a
+| (Nat.succ n'), f => λ a => f (compn' n' f a)
+
+def compn {α : Type}: Nat → (α → α) → (α → α)
+| Nat.zero, f => λ a => a
+| (Nat.succ n'), f => f ∘ (compn n' f)
+
+#eval (compn 5 Nat.succ) 0
+def sq (n : Nat) := n * n
 #eval (compn 5 sq) 2
+
+
+
 
 /- !inductive List (α:Type u) where
  | nil: List α
@@ -21,14 +43,61 @@ def compn {α : Type}:(α → α)-> Nat ->(α → α)
  def e :List Nat := List.nil
  def e':List Nat := []    -- List NOTATION
 
- def l1: List Nat :=List.cons 1 e
- def l1': List Nat :=1::e  -- notation for cons
- def l1'': List Nat :=[1]
+ def l1: List Nat := List.cons 1 e
+ def l1': List Nat := 1::e  -- notation for cons
+ def l1'': List Nat := [1]
 
- def l2 : List Nat :=List.cons 0 l1
+ def l2 : List Nat := List.cons 0 l1
  #reduce l2
 
  def list_len {α : Type} : List α -> Nat
  | List.nil => 0
- | (List.cons h t) =>1 + list_len t
+ | (List.cons h t) => 1 + list_len t
  #eval list_len l2
+
+
+
+/-!
+
+-/
+
+--
+def add : Nat → Nat → Nat
+| n, 0 => n
+| n, (m' + 1) => Nat.succ (add n m')
+
+#eval add 4 5
+
+/-!
+n * m
+n + (n * (m - 1))
+-/
+def mul : Nat → Nat → Nat
+| n, 0 => 0
+| n, (m' + 1) => add n (mul n m')
+
+#check Nat.mul
+#eval mul 5 3
+
+/-!
+n^m
+m = 0 => 0
+m != 0 => n * (n^(m-1))
+-/
+def exp : Nat → Nat → Nat
+| n, 0 => 1
+| n, (m' + 1) => mul n (exp n m')
+
+#eval exp 2 3
+
+
+-- empty
+#check @Empty
+
+#check @Unit
+
+#check @Bool
+
+#check @Int
+
+#check @String
