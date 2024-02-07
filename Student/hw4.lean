ee84db228a7c307ff21ec9ba6af1a182a790a528
LiuZhe <lzhe72537@gmail.com>
2024-02-07 16:56:13 +0000

    hw4 lecture notes

--- Student/hw4.lean
+++ Student/hw4.lean
@@ -1,6 +1,73 @@
+/-!
+------------------------------ HW4 starts here ----------------------------------------------------------------
+-/
+def isEvenLen : String → Bool := λ s => s.length % 2 == 0
+
+def combine : String → Bool → Bool := λ s b => (isEvenLen s) && b
+
+def foldr {α β : Type} : (α → β → β) → β → List α → β
+| _, id, List.nil => id
+| op, id, (h::t) => op h (foldr op id t)
+
+#reduce foldr combine true ["1234", "12345"] --should be false
+#reduce foldr combine true ["1234", "1234"] --should be true
+#reduce foldr combine true ["", "", ""] --should be true
+#reduce foldr combine true ["", "", "1"] --should be false
+/-!
+------------------------------ HW4 ends here ----------------------------------------------------------------
+-/
+
+
+/-!
+HW4 ans
+(lecture notes of hw4 class)
+-/
+def combine_ans : String → Bool → Bool
+| s, b => (isEvenLen s) && b
+
+#eval foldr combine_ans true []
+
+def foldr_ {α : Type} : (α → α → α) → α → List α → α
+| _, id, List.nil => id
+| op, id, (h::t) => op h (foldr op id t)
+
+#eval foldr_ (Nat.add) 0 [1, 2, 3]
+#eval foldr_ (Nat.add) 1 [1, 2, 3]
+
+
+-- what can go wrong:
+-- We can pass a non-identity element
+
+-- rules
+-- 1. id is the identiyt element for op
+-- 2. op must be associative
+
+/-!
+solution:
+to prove, for any a in α,
+(op a id = a) ∧
+(op id a = a)
+Then id is the identity element for op
+-/
+
+-- "structure" for defining a data type with just single constructor
+-- "mk" is the default name of the constructor
+-- can access the attributes using my_monoid.op, my_monoid.id, etc. like OOD
+structure my_monoid (α : Type) where
+(op : α → α → α)
+(id : α)
+(left_id : ∀ (a : α), op id a = a)
+(right_id : ∀ (a : α), op a id = a)
+
+
+def  a_monoid : my_monoid Nat := my_monoid.mk Nat.add 0 sorry sorry
+
+#check a_monoid.op
+
 
 /-!
 New Material
+(Lecture notes of hw3 class)
 -/
 
 /-!
@@ -21,11 +88,6 @@ def foldr' { α : Type } : (α → α → α) → α → List α → α
 | _, id, [] => id
 | op, id, h::t => op h (foldr' op id t)
 
--- def combine : String → Bool → Bool := _
-
--- def foldr { α β : Type } : (α → β → β) → β → List α → β
--- | op, id, List.nil => id
--- | op, id, h::t =>
 
 #reduce folder''' Nat.add 0 [1, 2, 3, 4, 5]
 #reduce folder''' Nat.mul 1 [1, 2, 3, 4, 5]
@@ -44,15 +106,3 @@ def fold_str : (String → String → String) → String → List String → Str
 | op, id, h::t => op h (fold_str op id t)
 
 #eval fold_str String.append "" ["q", "w"]
-
-/-!
-
--/
--- def foldr : (String → Bool → Bool) → Bool → List String → Bool
-
-
-
-/-!
-Type universe example of identity function,
-
--/
