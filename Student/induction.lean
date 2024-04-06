466bc0b0b7e0646b9967ff9eac11bc99306bf0f7
LiuZhe <lzhe72537@gmail.com>
2024-04-06 17:39:04 +0000

    update

--- Student/induction.lean
+++ Student/induction.lean
@@ -4,6 +4,21 @@
 
 
 #check Bool.rec
+/-!
+Bool.rec.{u}
+  {motive : Bool → Sort u}
+  (false : motive false)
+  (true : motive true)
+  (t : Bool) :
+  motive t
+-/
+
+example : ∀ (b : Bool), !!b = b :=
+by
+  intros b
+  inductive b
+  repeat { rfl }
+
 #check Nat.rec
 #check List.rec
 
